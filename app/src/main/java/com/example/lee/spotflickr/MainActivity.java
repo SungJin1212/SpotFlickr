420762f82ed7ec3c6b435e8d4e01e2de0475a373
ouiqdmw <ouiqdmw17@gmail.com>
2019-05-05 22:06:14 +0900

    split GalleryActivity from MainActivity

--- app/src/main/java/com/example/lee/spotflickr/MainActivity.java
+++ app/src/main/java/com/example/lee/spotflickr/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lee.spotflickr;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Environment;
 import android.provider.OpenableColumns;
 import android.support.v7.app.AppCompatActivity;
@@ -13,14 +14,15 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.Toast;
+
+import com.example.lee.spotflickr.Gallery.GalleryActivity;
+import com.example.lee.spotflickr.Gallery.GalleryAdapter;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -29,216 +31,38 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button btn;
-    int PICK_IMAGE_MULTIPLE = 1;
-    String imageEncoded;
-    List<String> imagesEncodedList;
-    private GridView gvGallery;
-    private GalleryAdapter galleryAdapter;
+    private Button btnGallery;
 
-    // custom gallery test
-    File mediaStorageDir;
-    private static String basePath;
-    private String[] imgs;
-
-    public boolean copyFileFromUri(Context context, Uri fileUri, String filename)
-    {
-        InputStream inputStream = null;
-        OutputStream outputStream = null;
-
-        try
-        {
-            ContentResolver content = context.getContentResolver();
-            inputStream = content.openInputStream(fileUri);
-
-            File root = Environment.getExternalStorageDirectory();
-            if(root == null){
-                Log.d("debug", "Failed to get root");
-            }
-
-            // create a directory
-            File saveDirectory = mediaStorageDir;
-            outputStream = new FileOutputStream( saveDirectory + "/"+filename); // filename.png, .mp3, .mp4 ...
-            if(outputStream != null){
-                Log.e( "debug", "Output Stream Opened successfully");
-            }
-
-            byte[] buffer = new byte[1000];
-            int bytesRead = 0;
-            while ( ( bytesRead = inputStream.read( buffer, 0, buffer.length ) ) >= 0 )
-            {
-                outputStream.write( buffer, 0, buffer.length );
-            }
-        } catch ( Exception e ){
-            Log.e( "debug", "Exception occurred " + e.getMessage());
-        } finally{
-
-        }
-        return true;
+    private void setUI() {
+        //initializing views
+        btnGallery = (Button) findViewById(R.id.buttonGallery);
     }
 
-    protected boolean loadCustomGallery() {
-        File file = mediaStorageDir;
-        imgs = file.list();
-        ArrayList<Uri> mArrayUri = new ArrayList<Uri>();
-        for(int i=0; i<imgs.length; i++){
-            Log.d("Debug","HJ debug success file count-"+i+": "+imgs[i]);
-            Uri mImageUri = Uri.fromFile(new File(mediaStorageDir.getPath()+"/"+imgs[i]));
-
-            mArrayUri.add(mImageUri);
-        }
-        galleryAdapter = new GalleryAdapter(getApplicationContext(),mArrayUri);
-        gvGallery.setAdapter(galleryAdapter);
-        gvGallery.setVerticalSpacing(gvGallery.getHorizontalSpacing());
-        ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) gvGallery
-                .getLayoutParams();
-        mlp.setMargins(0, gvGallery.getHorizontalSpacing(), 0, 0);
-        return true;
+    private void setButton() {
+        //button click event
+        btnGallery.setOnClickListener(this);
     }
 
+    private void init() {
+        setUI();
+        setButton();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // App.을 실행하자 마자 지정한 경로의 생성 및 접근에 용이하도록 아래와 같이 생성
-        mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), "MyCameraApp");
-        // This location works best if you want the created images to be shared
-        // between applications and persist after your app has been uninstalled.
-
-        // Create the storage directory if it does not exist
-        if (! mediaStorageDir.exists()){
-            Log.d("Debug","success HJ Debug: no dir exists, try create...");
-            if (! mediaStorageDir.mkdirs()){
-                Log.d("MyCameraApp", "failed to create directory");
-//                return null;
-            }
-        } else {
-            Log.d("Debug","success HJ Debug: mediaStorageExists");
-        }
-        basePath = mediaStorageDir.getPath();
-        Log.d("Debug","success HJ Debug: BasePath: "+basePath);
-
-        // setup objects
-        btn = findViewById(R.id.btn);
-        gvGallery = (GridView)findViewById(R.id.gv);// activity_main.xml에서 선언한 Gallery를 연결
-
-        loadCustomGallery();
-
-        //galleryAdapter = new GalleryAdapter(getApplicationContext(), basePath);
-
-        // register intent for normal gallery -> custom gallery copy intent
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent,"Select Picture"), PICK_IMAGE_MULTIPLE);
-            }
-        });
+        init();
     }
-
-    public String getFileName(Uri uri) {
-        String result = null;
-        if (uri.getScheme().equals("content")) {
-            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
-            try {
-                if (cursor != null && cursor.moveToFirst()) {
-                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
-                }
-            } finally {
-                cursor.close();
-            }
-        }
-        if (result == null) {
-            result = uri.getPath();
-            int cut = result.lastIndexOf('/');
-            if (cut != -1) {
-                result = result.substring(cut + 1);
-            }
-        }
-        return result;
-    }
-
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        try {
-            // When an Image is picked
-            Log.d("Debug","success1");
-            if (requestCode == PICK_IMAGE_MULTIPLE && resultCode == RESULT_OK
-                    && null != data) {
-                // Get the Image from data
-
-                Log.d("Debug","success2");
-                String[] filePathColumn = { MediaStore.Images.Media.DATA };
-                imagesEncodedList = new ArrayList<String>();
-                if(data.getData()!=null){
-
-                    Uri mImageUri=data.getData();
-                    if(copyFileFromUri(getApplicationContext(), mImageUri, getFileName(mImageUri))) {
-                        Log.d("Debug","file copy success");
-                    }
-
-                    Log.d("Debug","success"+mImageUri.getPath());
-                    // Get the cursor
-                    Cursor cursor = getContentResolver().query(mImageUri,
-                            filePathColumn, null, null, null);
-                    // Move to first row
-                    cursor.moveToFirst();
-
-                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                    imageEncoded  = cursor.getString(columnIndex);
-                    cursor.close();
-
-                    loadCustomGallery();
-
-                } else {
-
-                    Log.d("Debug","success3");
-                    if (data.getClipData() != null) {
-                        ClipData mClipData = data.getClipData();
-                        ArrayList<Uri> mArrayUri = new ArrayList<Uri>();
-                        for (int i = 0; i < mClipData.getItemCount(); i++) {
-
-                            ClipData.Item item = mClipData.getItemAt(i);
-                            Uri uri = item.getUri();
-                            mArrayUri.add(uri);
-                            Log.d("Debug","success"+uri.getPath());
-                            // Get the cursor
-                            Cursor cursor = getContentResolver().query(uri, filePathColumn, null, null, null);
-                            // Move to first row
-                            cursor.moveToFirst();
-
-                            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                            imageEncoded  = cursor.getString(columnIndex);
-                            imagesEncodedList.add(imageEncoded);
-                            cursor.close();
-
-                            galleryAdapter = new GalleryAdapter(getApplicationContext(),mArrayUri);
-                            gvGallery.setAdapter(galleryAdapter);
-                            gvGallery.setVerticalSpacing(gvGallery.getHorizontalSpacing());
-                            ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) gvGallery
-                                    .getLayoutParams();
-                            mlp.setMargins(0, gvGallery.getHorizontalSpacing(), 0, 0);
-
-                        }
-                        Log.v("LOG_TAG", "Selected Images" + mArrayUri.size());
-                    }
-                }
-            } else {
-                Toast.makeText(this, "You haven't picked Image",
-                        Toast.LENGTH_LONG).show();
-            }
-        } catch (Exception e) {
-            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
-                    .show();
+    public void onClick(View view) {
+        if (view == btnGallery) {
+            userGallery();
         }
-
-        super.onActivityResult(requestCode, resultCode, data);
+    }
+    private void userGallery() {
+        finish();
+        startActivity(new Intent(this, GalleryActivity.class));
     }
 }
