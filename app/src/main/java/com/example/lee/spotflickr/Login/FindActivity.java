dbe246270bfc5e8c905ad5ae72315d1786054c86
SungJin1212 <tjdwls1201@gmail.com>
2019-06-16 16:34:10 +0900

    Update FindActivity.java

--- app/src/main/java/com/example/lee/spotflickr/Login/FindActivity.java
+++ app/src/main/java/com/example/lee/spotflickr/Login/FindActivity.java
@@ -4,12 +4,15 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.lee.spotflickr.PopUps.PopUp;
 import com.example.lee.spotflickr.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -56,23 +59,37 @@ public class FindActivity extends AppCompatActivity implements View.OnClickListe
         btnBack.setOnClickListener(this);
     }
 
+    public void showNoticeDialog(String title, String message) {
+        // Create an instance of the dialog fragment and show it
+        DialogFragment dialog = PopUp.newInstance(title, message);
+        dialog.show(getSupportFragmentManager(), "LoginFragment");
+    }
+
     @Override
     public void onClick(View view) {
         if (view == btnFind) {
-            progressDialog.setMessage("처리중입니다. 잠시 기다려 주세요...");
+            String emailAddress = editTextUserEmail.getText().toString().trim();
+
+            if (TextUtils.isEmpty(emailAddress)) {
+                showNoticeDialog("Finding Password Error", "Please enter your current password");
+                return;
+            }
+            progressDialog.setMessage("Please wait...");
             progressDialog.show();
             //비밀번호 재설정 이메일 보내기
-            String emailAddress = editTextUserEmail.getText().toString().trim();
+
+
+
             firebaseAuth.sendPasswordResetEmail(emailAddress)
                     .addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
                             if (task.isSuccessful()) {
-                                Toast.makeText(FindActivity.this, "이메일을 보냈습니다.", Toast.LENGTH_LONG).show();
+                                Toast.makeText(FindActivity.this, "Success send email.", Toast.LENGTH_LONG).show();
                                 finish();
                                 startActivity(new Intent(getApplicationContext(), LoginActivity.class));
                             } else {
-                                Toast.makeText(FindActivity.this, "메일 보내기 실패!", Toast.LENGTH_LONG).show();
+                                Toast.makeText(FindActivity.this, "Incorrect email.", Toast.LENGTH_LONG).show();
                             }
                             progressDialog.dismiss();
                         }
